9161033e58ca2bdad9e96cdef95afb0a7669da3f
lqb1996 <lqb1139134758@163.com>
2018-04-07 20:35:18 +0800

    share

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -138,7 +138,7 @@ class UserController extends Controller
     }
 
     public function shareIndex(LeafsnapRes $leafsnapRes){
-        $leafsnapRes = LeafsnapRes::where('log_id', $leafsnapRes->id);
-        return compact('leafsnapRes', 'imgUrl');
+        $leafsnapRes = LeafsnapRes::where('log_id', $leafsnapRes->id)->get();
+        return compact('leafsnapRes');
     }
 }
