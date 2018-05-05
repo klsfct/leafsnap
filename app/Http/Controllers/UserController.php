eafad2c671c256c45f96a3b26f14ef878d20916d
lqb1996 <lqb1139134758@163.com>
2018-05-05 17:05:29 +0800

    descrition

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -185,7 +185,7 @@ class UserController extends Controller
         $id = LeafsnapRes::insertGetId($params);
         $res = json_decode($res, true);
 
-        if(is_array($res['result'])) {
+        if(is_array($res['result'][0])) {
             foreach ($res['result'] as &$plant) {
                 $plant['desList'] = $this->searchBySolr($plant['name']);
             }
